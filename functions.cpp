e1e3792a733d85cf6387b45281951faa77b8eff7
KarenMarkosyan <priboy34@mail.ru>
2015-08-16 13:31:12 +0300

    Написаны прототипы используемых вункций.
    Написаны прототипы функций для работы программы. Все прототипы содержат
    коментарий.

---
+++ functions.cpp
@@ -0,0 +1,61 @@
+#include "functions.h"
+
+
+bool readText(const QString &fileName, const QString &text)
+{
+
+}
+
+
+bool readRules(const QString &fileName, const RulesMap &rules)
+{
+
+}
+
+
+bool isCorrectText(const QString &text)
+{
+
+}
+
+
+bool isCorrectRules(const RulesMap &rules)
+{
+
+}
+
+
+int textToWords(const QString &text, QList<Word> &words)
+{
+
+}
+
+
+int wordsToTexts(const QString &text, const QList<Word> words, QList<QString> &texts)
+{
+
+}
+
+
+void analyze(Word &word, const RulesMap &rules, QString bufer, int start, int end)
+{
+
+}
+
+
+void analyzeAllWords(QList<Word> &words, const RulesMap &rules)
+{
+
+}
+
+
+bool writeTexts(const QString &fileName, const QList<QString> &texts)
+{
+
+}
+
+
+RulesMap createRulesMap(const QString &s)
+{
+
+}
